14c00854a5ca15d2dabbe2a3f4dda0826b124544
ignormies <bryce.beagle@gmail.com>
2024-05-05 22:39:30 -0700

    Print info on module import

--- src/lib.rs
+++ src/lib.rs
@@ -16,8 +16,26 @@ impl FooBar {
 }
 
 /// A Python module implemented in Rust.
+// Using experimental experimental-declarative-modules feature so we can use pymodule_init
 #[pymodule]
-fn what(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_class::<FooBar>()?;
-    Ok(())
+mod what {
+    use super::*;
+
+    #[pymodule_export]
+    use super::FooBar;
+
+    #[pymodule_init]
+    fn init(_m: &Bound<'_, PyModule>) -> PyResult<()> {
+        // Arbitrary code to run at the module initialization
+        println!("libwhat.so imported!");
+        Ok(())
+    }
+
 }
+
+// #[pymodule]
+// fn what(m: &Bound<'_, PyModule>) -> PyResult<()> {
+//     m.add_class::<FooBar>()?;
+//     Ok(())
+// }
+
